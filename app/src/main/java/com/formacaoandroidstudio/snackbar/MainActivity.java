e330ba1291398db9cfe7711cbf844450179be947
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-24 21:40:12 -0300

    Snackbar

---
+++ app/src/main/java/com/formacaoandroidstudio/snackbar/MainActivity.java
@@ -0,0 +1,57 @@
+package com.formacaoandroidstudio.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // função make recebe dois parâmetros: (1) view (2) valor da msg (3) duração da SnackBar
+                Snackbar.make(view, "Visualizando SnackBar", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
+/* Anotações:
+* Antes de poder usar as Snackbar, você precisa adicionar uma dependência à biblioteca Material Components para Android.
+* Você pode adicionar uma ação a um Snackbar, permitindo que o usuário responda à sua mensagem. Se você adicionar uma ação a um
+* Snackbar, o Snackbar colocará um botão ao lado do texto da mensagem. O usuário pode acionar sua ação pressionando o botão.
+* Por exemplo, um app de e-mails pode colocar um botão desfazer na mensagem "e-mail arquivado". Se o usuário clicar no botão desfazer,
+* o app removerá o e-mail do arquivo.
+*
+* Para adicionar uma ação a uma mensagem do Snackbar, você precisa definir um objeto listener que implemente a interface
+* View.OnClickListener. O sistema chamará o método onClick() do listener se o usuário clicar na ação da mensagem.
+*
+* O Snackbar fornece uma estática 'make' que é o método para produzir uma Snackbar configurada da maneira desejada. Esse método leva
+* como parametro uma 'View', que será usado para encontrar um ancestral adequado ViewGroup para exibir a barra de Snackbar, uma string
+* de texto para exibir a mensagem e uma duração para exibir a barra de snackbar. Um ancestral adequado ViewGroup será o mais próximo
+* CoordenadorLayout para o View passado, ou a DecorView se nenhum pudesse ser encontrado.
+*
+* As predefinições de duração disponíveis são:
+*  -> LENGTH_INDEFINITE(Mostrar a barra de Snackbar até que ela seja descartada ou outra barra de Snackbar seja mostrada)
+*  -> LENGTH_LONG(Mostrar o snackbar por um longo período de tempo)
+* -> LENGTH_SHORT(Mostrar o snackbar por um curto período de tempo)
+*
+* Snackbars funcionam melhor se forem exibidos dentro de um CoordenadorLayout. CoordenadorLayout permite que a barra de Snackbar
+* habilite comportamentos como deslizar para dispensar, bem como mover automaticamente widgets como FloatingActionButton.
+*
+* Fonte: https://m2.material.io/components/snackbars/android#using-snackbars
+* */
